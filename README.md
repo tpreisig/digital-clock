a4d6c5ff10b11ddceea82ecb547d2f1af8db2ab4
Tiziano Preisig <tiziano.preisig@hispeed.ch>
2025-01-15 16:48:44 +0100

    completed coded and added all to code base

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# digital-clock
-Digital clock application in written in Python with PyQt6 of the Qt6 framework
+# Digital Clock
+![Build Status](https://img.shields.io/badge/build-passing-brightgreen)
+![License](https://img.shields.io/badge/license-MIT-blue.svg)
+![Version](https://img.shields.io/badge/version-1.0.0-orange)
+
+Desktop application written in Python with PyQt6.\
+--> PyQt6 is a comprehensive set of Python bindings for the Qt6 application framework, that enables you to create beautiful GUI application using Python.
+
+
+
+
+## Installation
+Keep the project in a virtual environment. This way you make sure, that the packages installed will not affect other projects or operating system’s packages. 
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/tpreisig/digital-clock.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd digital-clock
+   ```
+3. Create a virtual environment and activate it:
+   ```bash
+   python3 -m venv dcl-venv
+   source dcl-venv/bin/activate
+   ```
+4. Install the module PyQt6:
+   ```bash
+   pip install PyQt6
+   ```
+
+## Usage
+
+To start the application, run:
+```bash
+python3 -m main
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Screenshots
+
+![Screenshot](screenshots/digital_clock.png)
+
+## Contact
+
+Maintained by tpreisig - feel free to reach out!
--- main.py
+++ main.py
@@ -6,20 +6,25 @@ from PyQt6.QtGui import QLinearGradient, QBrush, QPalette
 class DigitalClock(QWidget):
     def __init__(self):
         super().__init__()
-        self.time_label = QLabel("06:06:06", self)
+        self.time_label = QLabel("16:40:20", self)
+        self.timer = QTimer(self)
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle("Digital Clock")
         self.setFixedSize(800, 400)
-
         vbox = QVBoxLayout()
         vbox.addWidget(self.time_label)
         self.setLayout(vbox)
 
         self.time_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        # linear gradient background
+        self.time_label.setStyleSheet("""
+            font-size: 120px;
+            font-family: 'Avenir';
+            color: aliceblue;
+            font-weight: 900;
+        """)
+        # Setting up the linear gradient background
         palette = QPalette()
         gradient = QLinearGradient(0, 0, self.width(), self.height())
         gradient.setColorAt(0.0, Qt.GlobalColor.darkBlue)
@@ -28,12 +33,17 @@ class DigitalClock(QWidget):
         palette.setBrush(QPalette.ColorRole.Window, brush)
         self.setPalette(palette)
 
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start(1000)
+        self.update_time()
+
+    def update_time(self):
+        current_time = QTime.currentTime().toString("hh:mm:ss")
+        self.time_label.setText(current_time)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    # ex.show() method inherited from QWidget class
     ex = DigitalClock()
+    # ex.show() inherits from QWidget
     ex.show()
     sys.exit(app.exec())
-
-
